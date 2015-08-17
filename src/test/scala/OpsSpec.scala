5fd22c95d017d0adfe74315c6918fe79b3ccaf9d
dsotolongo <dsotolongo@twitter.com>
2015-08-16 23:50:16 -0700

    c: add some real OpsSpec tests (failing)

--- src/test/scala/OpsSpec.scala
+++ src/test/scala/OpsSpec.scala
@@ -1,8 +1,5 @@
 package co.sortalon.fcon
 
-import Parsers.P
-import Resolver.{R, Resolved}
-import AST._
 import org.scalatest.{FunSuite, Matchers}
 import org.scalatest.prop.PropertyChecks
 import scala.util.Success
@@ -10,22 +7,34 @@ import scala.util.Success
 class OpsSpec extends FunSuite
     with Matchers
     with PropertyChecks {
+  import Parsers.P
+  import Resolver.R
+  import AST._
+  import generators.ASTs._
 
   test("two merged lists have n * m elements") {
-    forAll { (l1: List[String], l2: List[String]) =>
-      implicit val res = Resolved()
-      val lst1 = Lst(l1.map(Str(_)))
-      val lst2 = Lst(l2.map(Str(_)))
-      val Success(Lst(result)) = Ops.merge(List(lst1, lst2))
-      result should have length (l1.length * l2.length)
+    forAll { (l1: Lst[P], l2: Lst[P]) =>
+      val toMerge = List(l1, l2).map {
+        l => Resolver(l).get
+      }
+      val Success(Lst(result)) = Ops.merge(toMerge)
+      result should have length (l1.elems.length * l2.elems.length)
     }
   }
 
   // these need a traversal combinator
-  // test("a string on the left ends up at the end of every element")
-  // test("a string on the right ends up at the beginning of every element")
+  // test("a string on the right ends up at the end of every element")
+  // test("a string on the left ends up at the beginning of every element")
 
-  // test("a list merged with a non-list has n elements")
+  test("a list merged with a non-list has n elements") {
+    forAll { (l: Lst[P], n: Node[P]) =>
+      val toMerge = List(l, n).map {
+        x => Resolver(x).get
+      }
+      val Success(Lst(result)) = Ops.merge(toMerge)
+      result should have length (l.elems.length)
+    }
+  }
   // test("a dict merged with a dict has union(left, right) elements")
   // test("a dict merged with a non-dict has n elements")
   // test("an applied function has its arg bound in scope")
